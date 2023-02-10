1522ff2f9735e70fa0f4f68771dd3d66b7fdddf3
alijafari0572 <alijafari0572@gmail.com>
2023-02-10 16:54:13 +0330

    Add project files.

---
+++ CityinfoAPI/Controllers/CiriesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityinfoAPI.Controllers
+{
+    [ApiController]
+    public class CiriesController : ControllerBase
+    {
+        [HttpGet("api/Cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+            new {id=1,name="Tehran"},
+            new {id=2,name="shiraz"},
+            });
+        }
+    }
+}
