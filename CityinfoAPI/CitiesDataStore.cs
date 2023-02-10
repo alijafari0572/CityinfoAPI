90468f7e630a181e752db8abb10b843c2a37dda5
alijafari0572 <alijafari0572@gmail.com>
2023-02-10 19:07:02 +0330

    ساخت dto و اندیس برای دو تایع get

---
+++ CityinfoAPI/CitiesDataStore.cs
@@ -0,0 +1,19 @@
+﻿using CityinfoAPI.Models;
+
+namespace CityinfoAPI
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> cityDtos { get; set; }
+        public static CitiesDataStore current { get;}=new CitiesDataStore();
+        public CitiesDataStore()
+        {
+            cityDtos = new List<CityDto>(){
+
+                new CityDto(){Id=1,Name="tehran",Description="this is iran"},
+                new CityDto(){Id=1,Name="mashhad",Description="this is khorasan"},
+                new CityDto(){Id=1,Name="shiraz",Description="this is fars"},
+            };
+        }
+    }
+}
---
+++ CityinfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityinfoAPI.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]
+    [Route("api/Cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.current);
+        }
+        [HttpGet("{id}")]
+        public JsonResult GetCities(int id)
+        {
+            return new JsonResult(CitiesDataStore.current.cityDtos
+                .FirstOrDefault(c=>c.Id==id));
+        }
+    }
+}
