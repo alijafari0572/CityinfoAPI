656d1920fa5c65257c8934ea017529b4f75aee9b
alijafari0572 <alijafari0572@gmail.com>
2023-02-12 15:35:58 +0330

    جلسه 25

---
+++ src/Api/CityApi/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityinfoAPI.Controllers
+{
+    [Route("api/Cities")]
+    [ApiController]
+    //[Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.current.Cities);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCities(int id)
+        {
+            var city = CitiesDataStore.current.Cities
+                .FirstOrDefault(c => c.Id == id);
+            return Ok(city);
+        }
+    }
+}
---
+++ src/Api/CityApi/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityinfoAPI.Controllers
+{
+    [Route("api/cities/{cityid}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPointsOfInterst(int cityid) 
+        {
+            var city=CitiesDataStore.current.Cities.FirstOrDefault(c=>c.Id==cityid);
+            if (city==null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+
+
+        [HttpGet("{pointofinterestId}")]
+        public IActionResult GetPointOfInterst(int cityid,int pointofinterestId)
+        {
+            var city = CitiesDataStore.current.Cities.FirstOrDefault(c => c.Id == cityid);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var point=city.PointsOfInterest.FirstOrDefault(p=>p.Id==pointofinterestId);
+            if (point==null)
+            {
+                return NotFound();
+            }
+            return Ok(point);
+        }
+    }
+}
