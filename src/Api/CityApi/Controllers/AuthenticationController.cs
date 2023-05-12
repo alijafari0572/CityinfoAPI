a9aa3787ee1e21cfa06ce48fc26241c61934df0c
alijafari0572 <alijafari0572@gmail.com>
2023-05-12 20:26:30 +0330

    درس69

---
+++ src/Api/CityApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        public class AuthenticationRequestBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+
+        public class CityInfoUser
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+            public CityInfoUser(int userId, string userName, string firstName, string lastName, string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(AuthenticationRequestBody
+            authenticationRequestBody)
+        {
+            var user = ValidateUserCredentials(authenticationRequestBody.UserName,
+                authenticationRequestBody.Password);
+
+            return "";
+        }
+
+        private CityInfoUser ValidateUserCredentials(string? userName,
+            string? password)
+        {
+            return null;
+        }
+    }
+}
